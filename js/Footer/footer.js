b4a1151c9e8bade824df887de38ea1b14d80f832
osamasayed3344 <135232287+osamasayed3344@users.noreply.github.com>
2025-09-02 14:54:52 +0300

    Delete js directory

--- js/Footer/footer.js
+++
@@ -1,7 +0,0 @@
-export function Footer()
-{
-    let footer =document.querySelector("footer");
-    let p=document.createElement("p");
-    p.innerHTML="&copy; 2025 All rights reserved by Osama Sayed";
-    footer.appendChild(p);
-}
--- js/Slider/Right/right.js
+++
@@ -1,9 +0,0 @@
-export function RightSlider()
-{
-    const right=document.createElement("div");
-    right.className="right";
-    let img=document.createElement("img");
-    img.src="img/11zon_cropped.png";
-    right.append(img);
-    return right;
-}
